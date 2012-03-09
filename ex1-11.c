129ac22765c054c788350a0d657254192b2920aa
Steven Merrill <steven.merrill@gmail.com>
2012-03-09 08:31:26 -0500

    Add solution to Exercise 1-11.

---
+++ ex1-11.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+#define IN 0
+#define OUT 1
+
+main() {
+  int c, state;
+
+  state = OUT;
+
+  while ((c = getchar()) != EOF) {
+    if (c == ' ' || c == '\n' || c == '\t') {
+      if (state == IN) {
+        state = OUT;
+        putchar('\n');
+      }
+    }
+    else {
+      state = IN;
+      putchar(c);
+    }
+  }
+}
