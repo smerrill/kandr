e04ddd4464c1e217a13fb71bb89d4502978ecf5b
Steven Merrill <steven.merrill@gmail.com>
2012-03-09 08:12:53 -0500

    Assign in a more K&R way.

--- ex1-8.c
+++ ex1-8.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 
 main() {
-  int c;
-  int spaces = 0;
-  int tabs = 0;
-  int newlines = 0;
+  int c, spaces, tabs, newlines;
+  spaces = tabs = newlines = 0;
 
   while ((c = getchar()) != EOF) {
     switch (c) {
